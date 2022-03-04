85c9a3d6ba36f4fa1c0341ed109acf18592acec5
EnochSage <84729179+EnochSage@users.noreply.github.com>
2022-03-04 02:09:16 -1200

    Create Debugging_with_Laurel_answers.js

---
+++ Debugging_with_Laurel_answers.js
@@ -0,0 +1,108 @@
+// Collect the Diamonds
+/* part 2
+>>>>>>Fail   */
+moveForward();
+moveForward();
+moveForward();
+
+//success
+moveForward();
+collect();
+moveForward();
+collect();
+moveForward();
+collect();
+
+/* part 3
+>>>>>>fail */
+moveForward();
+collect();
+moveForward();
+collect();
+turnLeft();
+moveForward();
+collect();
+
+//success
+moveForward();
+collect();
+moveForward();
+collect();
+turnRight();
+moveForward();
+collect();
+
+/* part 4
+>>>>>>fail */
+moveForward();
+collect();
+moveForward();
+collect();
+
+//success
+moveForward();
+collect();
+collect();
+collect();
+
+/* part 5
+>>>>>>fail */
+moveForward();
+collect();
+turnLeft();
+moveForward();
+collect();
+turnRight();
+moveForward();
+collect();
+turnRight();
+moveForward();
+collect();
+
+//success
+moveForward();
+collect();
+turnRight();
+moveForward();
+collect();
+turnLeft();
+moveForward();
+collect();
+turnRight();
+moveForward();
+collect();
+
+
+/* part 6
+>>>>>>fail */
+moveForward();
+collect();
+moveForward();
+collect();
+moveForward();
+collect();
+moveForward();
+collect();
+collect();
+
+// success
+moveForward();
+collect();
+moveForward();
+collect();
+moveForward();
+collect();
+turnRight();
+moveForward();
+moveForward();
+collect();
+turnRight();
+moveForward();
+collect();
+moveForward();
+collect();
+
+
+/* part 7
+>>>>>>fail */
+
